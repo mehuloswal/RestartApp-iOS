5dae750758fb32e53132a357f7bdd5e5a177ed76
Mehul Oswal <mehuloswal21@gmail.com>
2022-07-29 18:34:02 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mehul Oswal on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
