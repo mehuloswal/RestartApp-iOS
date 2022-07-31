03347885fdcd2fc367400a4a7d22806bf2417989
Mehul Oswal <mehuloswal21@gmail.com>
2022-07-31 19:25:20 +0530

    Added Preference and Haptic Feedbacks

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -33,6 +33,7 @@ struct HomeView: View {
             Spacer()
                 Button(action:{
                     withAnimation{
+                        playSound(sound: "success", type: "m4a")
                         isOnboardingViewActive = true
                     }
             }){
--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -16,6 +16,8 @@ struct OnboardingView: View {
     @State private var indicatorOpacity : Double = 1.0
     @State private var textTitle : String = "Share."
     
+    let hapticFeedback = UINotificationFeedbackGenerator()
+    
     
     var body: some View {
         ZStack {
@@ -127,9 +129,12 @@ struct OnboardingView: View {
                         .onEnded { _ in
                             withAnimation(Animation.easeOut(duration: 0.4)){
                                 if buttonOffset > buttonWidth / 2 {
+                                    hapticFeedback.notificationOccurred(.success)
+                                    playSound(sound: "chimeup", type: "mp3")
                                     buttonOffset = buttonWidth - 80
                                     isOnboardingViewActive = false
                                 } else {
+                                    hapticFeedback.notificationOccurred(.warning)
                                     buttonOffset = 0
                                 }
                             }
@@ -150,6 +155,7 @@ struct OnboardingView: View {
         .onAppear(perform: {
             isAnimating = true
         })
+        .preferredColorScheme(.dark)
     }
 }
 
