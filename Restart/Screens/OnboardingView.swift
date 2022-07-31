84112373386fa1123aa3bcf0f5203ec5dd8f9371
Mehul Oswal <mehuloswal21@gmail.com>
2022-07-31 15:56:15 +0530

    Added Complex Gestures to the button

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width-80
+    @State private var buttonOffset: CGFloat = 0
     var body: some View {
         ZStack {
             Color("ColorBlue").ignoresSafeArea(.all,edges: .all)
@@ -50,7 +52,7 @@ struct OnboardingView: View {
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width:80)
+                            .frame(width:buttonOffset + 80)
                         Spacer()
                     }
                     HStack {
@@ -66,13 +68,27 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                     .frame(width: 80, height: 80, alignment: .center)
-                    .onTapGesture {
-                        isOnboardingViewActive = false
-                    }
+                    .offset(x:buttonOffset)
+                    .gesture(DragGesture()
+                        .onChanged{ gesture in
+                            if gesture.translation.width > 0 && buttonOffset<=buttonWidth - 80{
+                                buttonOffset = gesture.translation.width
+                            }
+                        }
+                        .onEnded { _ in
+                            if buttonOffset > buttonWidth / 2 {
+                                buttonOffset = buttonWidth - 80
+                                isOnboardingViewActive = false
+                            } else {
+                                buttonOffset = 0
+                            }
+                            
+                        }
+                    )
                         Spacer()
                     }
                 }
-                .frame( height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
                 
             }
